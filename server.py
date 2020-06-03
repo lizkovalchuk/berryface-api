da441adbd54724420f7b266f3bc908273b30e3c6
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2020-06-03 12:35:53 -0400

    testing verion control with namecheap

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Testing Git Pull</h1><p>Testing if I can push from laptop and pull on server</p>"
 
 
 
